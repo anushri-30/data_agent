31a9448056de8edd1c5e626f43b5d103c8464b24
anushri-30 <98702794+anushri-30@users.noreply.github.com>
2026-09-04 22:54:29 +0530

    Initial project setup

---
+++ utils/llm_pick.py
@@ -0,0 +1,41 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def pick_llm(level: str):
+    """
+    Picks the appropriate LLM based on the level of the question.
+
+    Args:
+        level (str): The level of the question, can be "low", "medium", or "high".
+
+    Returns:
+        ChatOpenAI: The LLM instance to be used.
+    """
+    if level.lower() == "low":
+        llm = ChatGroq(
+            model="qwen/qwen3.6-27b",
+            temperature=0
+        )
+
+    elif level.lower() == "medium":
+        llm = ChatGroq(
+            model="qwen/qwen3.6-27b",
+            temperature=0
+        )
+
+    elif level.lower() == "high":
+        llm = ChatGroq(
+            model="qwen/qwen3.6-27b",
+            temperature=0
+        )
+
+    else:
+        raise ValueError(f"Unsupported level: {level}")
+
+    return llm
+
+llm_obj = pick_llm("low")  
+print(llm_obj.invoke("What is the capital of France?"))
